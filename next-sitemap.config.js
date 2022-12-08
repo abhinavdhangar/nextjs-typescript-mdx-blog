d169795fd4588b33529e520570346a3a8e0f5fd1
abhinav <abhinavderapur0@gmail.com>
2022-12-08 19:49:20 +0000

    Update 7 files

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+const config = {
+    siteUrl: 'https://hiro.arcaned.live',
+    generateRobotsTxt: true, // (Optional parameter for creating robots.txt file)
+    // Other available options..
+    
+  }
+ 
+module.exports = config;
